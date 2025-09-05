1ea9f27e16dc759af4c65d7b6c391fba92cd2c2d
BIKESHOPROMEO <tama@realeyes.co.jp>
2025-09-05 19:26:20 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const date = new Date(dateStr);
     const day = date.getDay();
 
-    if (holiday_jp.siHoliday(date)) retaun "holiday";
+    if (holiday_jp.siHoliday(date)) return "holiday";
     if (day === 0) return "sunday";
     if (day === 6) return "saturday";
     return "";
